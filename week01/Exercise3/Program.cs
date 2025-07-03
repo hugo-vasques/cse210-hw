15ba5b18a68462158acf849bc866d3f7816dc72f
MaCK34 <prom23hugomarcelo.sa@gmail.com>
2025-07-03 18:36:03 -0500

    Completed the Excercise #3 plus the extra requirements

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            Random newRandom = new Random();
+            int magicNumber = newRandom.Next(1, 100);
+
+            int guess = -1;
+            int attempts = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                attempts++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+            Console.WriteLine("Nice! You guessed it!");
+            Console.WriteLine($"And the number of attempts is: {attempts}!");
+
+            Console.Write("Would you like to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        }
     }
 }
