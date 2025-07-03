2fa05f06fa2072a2d5a0edc618cf1109dd6d3a78
MaCK34 <prom23hugomarcelo.sa@gmail.com>
2025-07-03 12:49:53 -0500

    Completed he Exercise #2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Hi there pal :) please enter a number from 50 to 100 to proceed: ");
+        string userValue = Console.ReadLine();
+        int x = int.Parse(userValue);
+
+        string letter = "";
+
+        if (x >= 90)
+        {
+            letter = "A";
+        }
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+        else if (x >= 70)
+        {
+            letter = "C";
+        }
+        else if (x >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (x >= 70)
+        {
+            Console.WriteLine("Congrats! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Ooof, you didn't make it, better luck next time!");
+        }
     }
 }
