153483404d9c7d7280daa0a1e047d3655ffaf999
MaCK34 <prom23hugomarcelo.sa@gmail.com>
2025-07-17 22:26:45 -0500

    added a new .cs file that will display hidden characters

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+    private Random _random = new Random();
+
+    public Scripture(Reference reference, string text);
+    {
+        
+    }
+}
