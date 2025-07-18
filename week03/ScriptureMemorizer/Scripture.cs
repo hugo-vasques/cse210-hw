cc5b2b8b5896b053f92f793078cdf79644f9aea4
MaCK34 <prom23hugomarcelo.sa@gmail.com>
2025-07-17 22:45:13 -0500

    created a Reference file to display the book, chapter, verse and endverse

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -7,8 +7,50 @@ public class Scripture
     private List<Word> _words = new List<Word>();
     private Random _random = new Random();
 
-    public Scripture(Reference reference, string text);
+    public Scripture(Reference reference, string text)
     {
-        
+        _reference = reference;
+        string[] splitWords = text.Split(' ');
+        foreach (string word in splitWords)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int count)
+    {
+        int hidden = 0;
+        while (hidden < count)
+        {
+            int index = _random.Next(_words.Count);
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hidden++;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string result = _reference.GetDisplayText() + " ";
+        foreach (Word word in _words)
+        {
+            result += word.GetDisplayText() + " ";
+        }
+        return result;
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
