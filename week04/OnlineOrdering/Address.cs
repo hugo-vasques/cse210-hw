8b887897bfa98f5284aa2920940e82a52e4ad87c
MaCK34 <prom23hugomarcelo.sa@gmail.com>
2025-07-29 13:10:33 -0500

    added a Address class to store user's data

---
+++ week04/OnlineOrdering/Address.cs
@@ -0,0 +1,25 @@
+public class Address
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+
+    public Address(string street, string city, string state, string country)
+    {
+        Street = street;
+        City = city;
+        State = state;
+        Country = country;
+    }
+
+    public bool IsInUSA()
+    {
+        return Country.Trim().ToUpper() == "USA";
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{Street}\n{City}, {State}\n{Country}";
+    }
+}
