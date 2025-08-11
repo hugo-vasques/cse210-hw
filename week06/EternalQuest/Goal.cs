782e6dea78830c38dbe8c844c764bb6b97545714
MaCK34 <prom23hugomarcelo.sa@gmail.com>
2025-08-11 10:08:21 -0500

    created the abstract class Goal

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EternalQuestProgram
+{
+    abstract class Goal
+    {
+        protected string _shortName;
+        protected string _description;
+        protected int _points;
+
+        public Goal(string name, string description, int points)
+        {
+            _shortName = name.Replace("|", "/");
+            _description = description.Replace("|", "/");
+            _points = points;
+        }
+
+        public string ShortName => _shortName;
+        public string Description => _description;
+        public int Points => _points;
+
+        public abstract int RecordEvent();
+        public abstract bool IsComplete();
+        public abstract string GetDetailsString();
+        public abstract string GetStringRepresentation();
+    }
+}
