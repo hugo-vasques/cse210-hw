c9c204eee34f683e1800c725959120b0b7dfd0d0
MaCK34 <prom23hugomarcelo.sa@gmail.com>
2025-08-11 11:23:04 -0500

    3 classes that inherit from Goal

---
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -0,0 +1,43 @@
+class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+    public ChecklistGoal(string name, string description, int points, int target, int bonus, int amountCompleted = 0)
+        : base(name, description, points)
+    {
+        _target = target;
+        _bonus = bonus;
+        _amountCompleted = amountCompleted;
+    }
+
+    public override int RecordEvent()
+    {
+        if (_amountCompleted >= _target)
+        {
+            return 0;
+        }
+
+        _amountCompleted++;
+        int earned = _points;
+        if (_amountCompleted == _target)
+        {
+            earned += _bonus;
+        }
+
+        return earned;
+    }
+
+    public override bool IsComplete() => _amountCompleted >= _target;
+
+    public override string GetDetailsString()
+    {
+        return $"{(IsComplete() ? "[X]" : "[ ]")} {_shortName} -- {_description} (Completed {_amountCompleted}/{_target})";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"Checklist|{_shortName}|{_description}|{_points}|{_amountCompleted}|{_target}|{_bonus}";
+    }
+}
